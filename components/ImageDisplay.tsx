a2c2e2fb2e9a71a7efabb33a781308953c5075b6
gutielle <zgutielle@gmail.com>
2025-07-04 20:17:49 -0300

    Update ImageDisplay.tsx

--- components/ImageDisplay.tsx
+++ components/ImageDisplay.tsx
@@ -12,14 +12,6 @@ interface ImageDisplayProps {
 const ImageDisplay: React.FC<ImageDisplayProps> = ({ imageUrl, isLoading, error, prompt }) => {
   const downloadImage = () => {
     if (!imageUrl) return;
-
-    window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    event: 'baixar_desenho',
-    categoria: 'interacao',
-    acao: 'clique_botao_download',
-    label: prompt
-  });
   
     const link = document.createElement('a');
     const fileName = prompt.replace(/[^a-z0-9]/gi, '_').toLowerCase() || 'desenho_para_colorir';
@@ -28,6 +20,14 @@ const ImageDisplay: React.FC<ImageDisplayProps> = ({ imageUrl, isLoading, error,
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+
+      window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: 'baixar_desenho',
+    categoria: 'interacao',
+    acao: 'clique_botao_download',
+    label: prompt
+  });
 
   };
 
